73f2554b6752da69edfa8aa6eaf41debaf49a90a
monstered99 <monstered99@gmail.com>
2019-01-13 12:22:28 +0100

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,20 @@
+$(window).scroll(function(){
+
+  var wScroll = $(this).scrollTop();
+
+  
+
+});
+/*
+$('img').mouseenter(function(){
+  $(this).css({
+    'filter' : 'blur(5px)'
+  });
+});
+
+$('img').mouseout(function(){
+  $(this).css({
+    'filter' : 'blur(0px)'
+  });
+});
+*/
